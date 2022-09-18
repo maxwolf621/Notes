c1056501e9b1ce3dfc016cc94d340380d34388a1
maxwolf621 <68631186+maxwolf621@users.noreply.github.com>
2022-09-18 09:23:25 +0800

    add examples

--- JavaNote/javaStream/examples/src/main/java/com/streampractice/App.java
+++ JavaNote/javaStream/examples/src/main/java/com/streampractice/App.java
@@ -1,12 +1,10 @@
 package com.streampractice;
 
 import java.util.*;
-import java.util.function.Consumer;
-import java.util.stream.Stream;
-
-import lombok.extern.slf4j.Slf4j;
 public class App 
 {
+    private static final int Student = 0;
+
     public static void main( String[] args )
     {
    
@@ -18,6 +16,7 @@ public class App
         students.add(new Student(5, "e", 14, "female", 95));
         students.add(new Student(6, "f", 21, "male", 55));
         
+        /** 
         StudentOps studentOps = new StudentOps();
         studentOps.filterByAge(students);
         studentOps.filterByScore(students);
@@ -26,23 +25,35 @@ public class App
         studentOps.matchOps(students, 88);
         studentOps.skipAndLimit(students, 1, 4);
 
-        //Stream.of(students).flatMap(e).forEach(System.out.println);
-        //Stream<Student> stdStream = Stream.of(new Student(1, "a", 18, "male", 88), new Student(1, "a", 18, "male", 88));
-        //stdStream.forEach(System.out::println);
-        // Stream<Integer> stream2 = Stream.iterate(0, (x) -> x + 3).limit(5);
-        // stream2.forEach(System.out::println);
-        //Stream<Double> stream3 = Stream.generate(Math::random).limit(2);
-        //String a= Stream.of(3, 2, 3).map(String::valueOf).collect(Collectors.joining("," , "prefix ", " suffix"));
-        //System.out.println(a);
-        
+        Stream.of(students).flatMap(e).forEach(System.out.println);
+        Stream<Student> stdStream = Stream.of(
+            new Student(1, "a", 18, "male", 88), 
+            new Student(1, "a", 18, "male", 88));
+        stdStream.forEach(System.out::println);
 
+        Stream<Integer> stream2 = Stream.iterate(
+            0, (x) -> x + 3).limit(5);
+        stream2.forEach(System.out::println);
+
+        Stream<Double> stream3 = Stream.generate(
+            Math::random).limit(2);
+
+        String a = Stream.of(3, 2, 3).map(String::valueOf).collect(
+            Collectors.joining("," , "prefix ", " suffix"));
+        */
+
+
+        /* 
         List<Integer> reducingList = Arrays.asList(1, 2, 3, 4, 5);
         ReduceOps reduceOps = new ReduceOps();
         reduceOps.reduce(reducingList);
         reduceOps.reduceWithInitializer(reducingList, 0);
         reduceOps.reduceMultipleThreadsWithInitializer(reducingList, 0);
         reduceOps.reduceSequentialWithInitialize(reducingList, 0);
+        */
+        
 
+        /* 
         List<Fruit> fruitList = Arrays.asList(
             new Fruit("apple", 6),
             new Fruit("apple", 6),
@@ -51,7 +62,6 @@ public class App
             new Fruit("banana", 7), 
             new Fruit("grape",8)
         );
-
         FruitOps fruitOps = new FruitOps();
         fruitOps.groupingByFruitName(fruitList);
         fruitOps.groupingByFruitNameAndMapping(fruitList);
@@ -62,12 +72,11 @@ public class App
                 .mapToInt(Integer::parseInt) // 1,2,3
                 .max() // 3
                 .ifPresent(e -> System.out.println("max value : " + e));
-
+        */
         /**
          * Anonymous Class
          * Method Reference Static
          * Method Reference Instance
-         */
         List<String> strList = Arrays.asList("A","B","C");
         // using Anonymous class
         strList.forEach(new Consumer<String>() {
@@ -76,14 +85,27 @@ public class App
                 System.out.println("using anonymous class " + s); // ABC
             }
         });
-        // Method reference STATIC (CLASS_NAME::METHOD) 
+        private static void staticPrint(String s) {
+        System.out.println("Static Method " + s);
+        }
+        private void instancePrint(String s) {
+            System.out.println("instance method " + s);
+        }
+        */
+
+        /**
+         * Method reference STATIC (CLASS_NAME::METHOD) 
         strList.forEach(App::staticPrint); // ABC
-        // Method reference INSTANCE (INSTANCE::METHOD)
+        */
+
+        /** 
+         * Method reference INSTANCE (INSTANCE::METHOD)
         App main = new App();
         strList.forEach(main::instancePrint); // ABC
+        */
 
 
-
+        /**
         Arrays.asList("a1", "a2", "b1", "c2", "c1")
                 .parallelStream()
                 .filter(s -> {
@@ -102,12 +124,9 @@ public class App
                     return s1.compareTo(s2);
                 })
                 .forEach(s -> System.out.format("forEach: %s [%s]\n",
-                    s, Thread.currentThread().getName()));
-    }
-    private static void staticPrint(String s) {
-        System.out.println("Static Method " + s);
-    }
-    private void instancePrint(String s) {
-        System.out.println("instance method " + s);
+                    s, Thread.currentThread().getName()));*/
+
+                
+
     }
 }
--- JavaNote/javaStream/examples/src/test/java/com/streampractice/AppTest.java
+++ JavaNote/javaStream/examples/src/test/java/com/streampractice/AppTest.java
@@ -2,6 +2,13 @@ package com.streampractice;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 import org.junit.Test;
 
 /**
@@ -9,12 +16,64 @@ import org.junit.Test;
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void streamTest(){
+       Stream<String> s = IntStream.range(1, 4) // IntStream
+                .mapToObj(i -> "a" + i); // Stream<String>
+        s.forEach(System.out::println);
+
+        assertTrue(true);
+    }
+
+    @Test
+    public void intStreamParallel(){
+        IntStream.rangeClosed(1, 5).parallel().forEach(
+            System.out::println
+        );
+        assertTrue(true);
+    }
+
+    @Test
+    public void retainAllList(){
+        List<Integer> l = new ArrayList<>(Arrays.asList(1,2,3,4,5,6));
+        List<Integer> l2 = new ArrayList<>(Arrays.asList(1,2,3,4,7,8));
+        l.retainAll(l2);
+        l.forEach(System.out::println);
+    }
+
+    @Test
+    public void stringBuilderTest(){
+        StringBuilder str = new StringBuilder("GONE WRONG_01");
+        str.setCharAt( 0 , 'a' );
+        str.setLength(15);
+        str.chars().forEach(System.out::println);
+
+        System.out.println(str.length());
+
+        char[] array = new char[10];
+        Arrays.fill(array, '_');
+  
+        // get char from index 5 to 9
+        // and store in array start index 3
+        str.getChars(5, 10, array, 3);
+
+        Stream.of(array).forEach(System.out::print);
+    }
+
+    @Test
+    public void stringTrim(){
+        String s1 = " Learn Share Learn ";
+        String s2 = s1.trim(); // returns "Learn Share Learn"
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+
+    @Test public void entrySet(){
+        Map<Integer, String> sites = Map.of(
+            1,"Google",
+            2,"Youtube",
+            3,"Github"
+        );
+        sites.entrySet().forEach(System.out::println);
     }
 }
